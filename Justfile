894f1753baff95ca4c91a8afd5ca127e223fa0d8
Edward Jazzhands <ed.jazzhands@gmail.com>
2025-02-25 13:23:16 -0400

    Added node and NVM

--- Justfile
+++ Justfile
@@ -1,5 +1,11 @@
 build-local:
-	docker build -t python-toolkit -f Dockerfile_local_v2 .
+	docker build -t python-toolkit -f Dockerfile_local .
+
+sudo-build-local:
+	sudo docker build -t python-toolkit -f Dockerfile_local .
 
 build-remote:
 	docker build -t python-toolkit -f Dockerfile_remote .
+
+sudo-build-remote:
+	sudo docker build -t python-toolkit -f Dockerfile_remote .	
--- to_copy_in/py_help/main.py
+++ to_copy_in/py_help/main.py
@@ -6,8 +6,7 @@ console = Console()
 
 banner = figlet_format("Python Toolkit", font="smslant")
 
-welcome_message = f"""
-{'=' * 30}
+welcome_message = f"""{'=' * 30}
 [green]Python Toolkit Container[/green]
            by Edward Jazzhands
 
@@ -23,19 +22,29 @@ tools_table.add_column("Command\n", style="cyan", no_wrap=True)
 tools_table.add_column("Written in", style="magenta", no_wrap=True)
 tools_table.add_column("Purpose", style="green")
 
+## ! CHANGE THIS TO APP LIST TXT
+
 tools_table.add_row("git", "", "C", "Version Control")
 tools_table.add_row("curl", "", "C", "Mostly installing things")
 tools_table.add_row("uv", "", "Rust", "Manages all things python")
+tools_table.add_row("nvm", "", "Shell", "Node Version Manager for Javascript")
+tools_table.add_row("node", "", "C++", "Javascript runtime")
 tools_table.add_row("tmux", "", "C", "Screen multiplexer")
 tools_table.add_row("make", "", "C", "Build automation")
 tools_table.add_row("just", "", "Rust", "Command runner - replaces Make")
+tools_table.add_row("gulp", "", "Javascript", "Command runner for JS stuff")
+tools_table.add_row("neovim", "", "C", "Terminal text editor")
 tools_table.add_row("bat", "", "Rust", "cat with color")
 tools_table.add_row("fzf", "", "Go", "fuzzy finder")
 tools_table.add_row("ripgrep", "rg", "Rust", "modern version of grep")
 tools_table.add_row("black", "", "Python", "formatting")
 tools_table.add_row("rich-cli", "rich", "Python", "Syntax highlighting in terminal")
+tools_table.add_row("neovim", "nvim", "C", "Terminal-based text/code editor")
+tools_table.add_row("ducktools-pytui", "pytui", "Python", "Experimental Python managing TUI")
+tools_table.add_row("harlequin", "", "Python", "SQLite database viewer and editor")
 
-console.print(banner)
+
+console.print(f"\n[cyan]{banner}[/cyan]")
 console.print(welcome_message)
 console.print(tools_table)
 console.print("[italic]Command is same as name if blank[/italic]")
