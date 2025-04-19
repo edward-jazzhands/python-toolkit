b1de0e3546776219a229d5a03571c43010a41646
Edward Jazzhands <ed.jazzhands@gmail.com>
2025-04-19 17:12:18 -0300

    version 0.2.0

--- to_copy_in/.bashrc
+++ to_copy_in/.bashrc
@@ -1,15 +1,42 @@
 export LANG=C.UTF-8
 export LC_ALL=C.UTF-8
+export UV_LINK_MODE=copy
 
 # Add local bin to PATH (This is mostly used by uv, pipx, and other global tools)
 . "$HOME/.local/bin/env"
 
-tkhelp() {
-    (cd ~/.py_help && uv run main.py)
-}
 
 alias proj="cd ~/workspace/vscode-projects"
 alias ls="ls -lFa --color=auto"
 alias bat="batcat"
 
 echo "Type 'tkhelp' (Tool-Kit Help) to get started."
+
+# FUNCTIONS
+
+tkhelp() {
+    (cd ~/.py_help && uv run main.py)
+}
+
+# fuzzy cd
+fcd() {
+  local dir
+  dir=$(find . -type d -not -path '*/\.*' | fzf) && cd "$dir"
+}
+
+# fuzzy shell history
+fsh() {
+  eval "$(history | fzf | sed 's/ *[0-9]* *//')"
+}
+
+# fuzzy nano
+fnano() {
+  local file
+  file=$(find . -type f -not -path '*/\.*' | fzf) && ${EDITOR:-nano} "$file"
+}
+
+# fuzzy batcat
+fbat() {
+  local file
+  file=$(find . -type f -not -path '*/\.*' | fzf) && bat "$file"
+}
--- to_copy_in/py_help/main.py
+++ to_copy_in/py_help/main.py
@@ -14,7 +14,10 @@ OS Base: [cyan]Debian Slim[/cyan]
 Container Version: [cyan]0.1[/cyan]
 {'=' * 30}"""
 
-### Tools table
+###############
+# TOOLS TABLE #
+###############
+
 tools_table = Table(title="Included CLI and TUI Tools")
 
 tools_table.add_column("Name", style="cyan", no_wrap=True)
@@ -22,30 +25,46 @@ tools_table.add_column("Command\n", style="cyan", no_wrap=True)
 tools_table.add_column("Written in", style="magenta", no_wrap=True)
 tools_table.add_column("Purpose", style="green")
 
-## ! CHANGE THIS TO APP LIST TXT
-
 tools_table.add_row("git", "", "C", "Version Control")
 tools_table.add_row("curl", "", "C", "Mostly installing things")
 tools_table.add_row("uv", "", "Rust", "Manages all things python")
+tools_table.add_row("npm", "", "Javascript", "Node Package Manager")
 tools_table.add_row("nvm", "", "Shell", "Node Version Manager for Javascript")
 tools_table.add_row("node", "", "C++", "Javascript runtime")
 tools_table.add_row("tmux", "", "C", "Screen multiplexer")
 tools_table.add_row("make", "", "C", "Build automation")
 tools_table.add_row("just", "", "Rust", "Command runner - replaces Make")
-tools_table.add_row("gulp", "", "Javascript", "Command runner for JS stuff")
-tools_table.add_row("neovim", "", "C", "Terminal text editor")
-tools_table.add_row("bat", "", "Rust", "cat with color")
+tools_table.add_row("gulp", "", "Javascript", "Command runner based on node")
+tools_table.add_row("nano", "", "C", "Terminal text editor")
+tools_table.add_row("batcat", "bat", "Rust", "cat with color")
 tools_table.add_row("fzf", "", "Go", "fuzzy finder")
 tools_table.add_row("ripgrep", "rg", "Rust", "modern version of grep")
 tools_table.add_row("black", "", "Python", "formatting")
 tools_table.add_row("rich-cli", "rich", "Python", "Syntax highlighting in terminal")
-tools_table.add_row("neovim", "nvim", "C", "Terminal-based text/code editor")
 tools_table.add_row("ducktools-pytui", "pytui", "Python", "Experimental Python managing TUI")
 tools_table.add_row("harlequin", "", "Python", "SQLite database viewer and editor")
 
+###############
+# FZF TABLE #
+###############
+
+fzf_table = Table(title="FZF shortcut functions")
+
+fzf_table.add_column("Command\n", style="cyan", no_wrap=True)
+fzf_table.add_column("Purpose", style="green")
+
+fzf_table.add_row("fcd", "Fuzzy cd into a directory")
+fzf_table.add_row("fsh", "Fuzzy shell history")
+fzf_table.add_row("fnano", "Fuzzy nano into a file")
+fzf_table.add_row("fbat", "Fuzzy batcat into a file")
+
+###################
+# WELCOME MESSAGE #
+###################
 
 console.print(f"\n[cyan]{banner}[/cyan]")
 console.print(welcome_message)
 console.print(tools_table)
-console.print("[italic]Command is same as name if blank[/italic]")
+console.print("[italic]Command is same as name if blank[/italic] \n")
+console.print(fzf_table)
 console.print("\nType [cyan]tkhelp[/cyan] (Tool-Kit help) to display this message again.\n")
