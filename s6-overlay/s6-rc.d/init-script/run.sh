91032a60b4baf56e136f0d51b6a8dcc176310c20
Edward Jazzhands <ed.jazzhands@gmail.com>
2025-11-26 08:32:55 -0800

    new faster method of chowning home dir

--- s6-overlay/s6-rc.d/init-script/run.sh
+++ s6-overlay/s6-rc.d/init-script/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# set -eu will cause exit immediately if any command fails
+set -eu
 
 # Get UID/GID from environment variables (default to 568)
 PUID=${PUID:-568}
@@ -7,18 +9,33 @@ PGID=${PGID:-568}
 # Only modify user/group if they differ from current values
 CURRENT_UID=$(id -u devuser)
 CURRENT_GID=$(id -g devuser)
+HOME_DIR="/home/devuser"
 
 if [ "$PUID" != "$CURRENT_UID" ] || [ "$PGID" != "$CURRENT_GID" ]; then
-    echo "Updating devuser UID to $PUID and GID to $PGID"
-    
-    # Modify group ID
-    groupmod -o -g "$PGID" devuser
-    
-    # Modify user ID
-    usermod -o -u "$PUID" devuser
+
+    if [ "$CURRENT_UID" != "$PUID" ]; then
+        echo "Changing UID from $CURRENT_UID to $PUID"
+        usermod -u "$PUID" devuser
+    else
+        echo "Using default UID of $PUID"
+    fi
+
+    if [ "$CURRENT_GID" != "$PGID" ]; then
+        echo "Changing GID from $CURRENT_GID to $PGID"
+        groupmod -g "$PGID" devuser
+    else
+        echo "Using default GID of $PGID"
+    fi
     
     # Update ownership of home directory and any other directories
-    chown -R "$PUID":"$PGID" /home/devuser
+    # REMOVED. TOO SLOW.
+    # chown -R "$PUID":"$PGID" /home/devuser
+
+    # New faster method:
+    # Selective chown: only operate on files that don't already match ownership.
+    find "$HOME_DIR" \( -not -uid "$PUID" -o -not -gid "$PGID" \) \
+    -exec chown "$PUID:$PGID" {} +
+
 fi
 
 if [ -n "$PASSWORD" ]; then
