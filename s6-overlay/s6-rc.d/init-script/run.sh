5d4b198905bdb9405b4fbe7eb4f1cce73b89594c
Edward Jazzhands <ed.jazzhands@gmail.com>
2025-11-26 15:42:47 -0800

    Improve speed of chowning home dir in init-script

--- s6-overlay/s6-rc.d/init-script/run.sh
+++ s6-overlay/s6-rc.d/init-script/run.sh
@@ -11,31 +11,45 @@ CURRENT_UID=$(id -u devuser)
 CURRENT_GID=$(id -g devuser)
 HOME_DIR="/home/devuser"
 
+
 if [ "$PUID" != "$CURRENT_UID" ] || [ "$PGID" != "$CURRENT_GID" ]; then
 
     if [ "$CURRENT_UID" != "$PUID" ]; then
         echo "Changing UID from $CURRENT_UID to $PUID"
-        usermod -u "$PUID" devuser
+        # OLD: This automatically activates a chown of the home dir and is slow
+        # usermod -u "$PUID" devuser
+        # NEW: This is faster, but requires a manual chown of the home dir:
+        sed -i "s/^devuser:x:[0-9]*:/devuser:x:$PUID:/" /etc/passwd
     else
         echo "Using default UID of $PUID"
     fi
 
     if [ "$CURRENT_GID" != "$PGID" ]; then
         echo "Changing GID from $CURRENT_GID to $PGID"
-        groupmod -g "$PGID" devuser
+        # OLD: same reason as above
+        # groupmod -g "$PGID" devuser
+        # NEW:
+        sed -i "s/^devuser:x:[0-9]*:/devuser:x:$PGID:/" /etc/group
     else
         echo "Using default GID of $PGID"
     fi
     
+    echo "Updating ownership of $HOME_DIR"
+
     # Update ownership of home directory and any other directories
     # REMOVED. TOO SLOW.
     # chown -R "$PUID":"$PGID" /home/devuser
+
+    START_TIME=$(date +%s)
 
     # New faster method:
     # Selective chown: only operate on files that don't already match ownership.
     find "$HOME_DIR" \( -not -uid "$PUID" -o -not -gid "$PGID" \) \
     -exec chown "$PUID:$PGID" {} +
 
+    END_TIME=$(date +%s)
+    ELAPSED_SECONDS=$((END_TIME - START_TIME))
+    echo "Time elapsed for chown of home dir: ${ELAPSED_SECONDS}s"
 fi
 
 if [ -n "$PASSWORD" ]; then
